52ed21eec104ac90f77785ebc0e93f09047312f4
sALTaccount <alotofcatz@gmail.com>
2023-04-23 05:55:37 -0700

    remake buttons without recalculating latents

--- gui_elements/latents_tab.py
+++ gui_elements/latents_tab.py
@@ -5,7 +5,19 @@ import gradio as gr
 import os
 
 
-def make_latents(model_path, config_path, buckets_path, batch, progress=gr.Progress()):
+def make_latents(model_path, config_path, buckets_path, progress=gr.Progress()):
+    try:
+        print('creating latents...')
+        config = json.load(open(config_path))
+
+        from encoder import encode
+        encode(model_path, buckets_path, config, progress=progress)
+    except Exception as e:
+        return f"Latents failed to create!\nReason: {e}"
+    return "Latents created!"
+
+
+def make_buckets(config_path, buckets_path, batch):
     try:
         # horribly hacky but oh well
         os.environ['SD_TRAINER_CONFIG_FILE'] = config_path
@@ -19,24 +31,23 @@ def make_latents(model_path, config_path, buckets_path, batch, progress=gr.Progr
         print('writing aspect buckets to file...')
         with open(buckets_path, 'wb') as f:
             pickle.dump(bucket, f)
-        print('creating latents...')
-        config = json.load(open(config_path))
-
-        from encoder import encode
-        encode(model_path, buckets_path, config, progress=progress)
     except Exception as e:
-        return f"Latents failed to create!\nReason: {e}"
-    return "Latents created!"
+        return f"Buckets failed to create!\nReason: {e}"
+    return "Buckets created!"
 
 
 def load():
     model_path = gr.Textbox(lines=1, label="Model Path", value="model", interactive=True)
     config_load_path = gr.Textbox(lines=1, label="Config Path", value="train_config.json", interactive=True)
-    pickle_save_path = gr.Textbox(lines=1, label="Buckets Save Path", value="buckets.pkl", interactive=True)
+    bucket_save_path = gr.Textbox(lines=1, label="Buckets Save Path", value="buckets.pkl", interactive=True)
+    buckets_button = gr.Button(value="Create Buckets")
     train_batch = gr.Number(label="Train Batch Size", value=1, interactive=True)
     latents_button = gr.Button(value="Create Latents")
     log_output = gr.components.Textbox(label="Log")
 
+    buckets_button.click(fn=make_buckets,
+                         inputs=[config_load_path, bucket_save_path, train_batch],
+                         outputs=[log_output])
     latents_button.click(fn=make_latents,
-                         inputs=[model_path, config_load_path, pickle_save_path, train_batch],
+                         inputs=[model_path, config_load_path, bucket_save_path],
                          outputs=[log_output])
