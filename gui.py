950738a703c5056b6e154eba49c7edec1c6d31a7
Andrew Powers-Holmes <aholmes@omnom.net>
2023-04-24 05:57:09 +0000

    make gui run on 7861 and bind-all by default

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ import gui_elements.data_tab as data_tab
 import gui_elements.latents_tab as latents_tab
 import gui_elements.train_tab as train_tab
 
-with gr.Blocks(theme='gstaff/xkcd') as gui:
+with gr.Blocks(theme="gstaff/xkcd") as gui:
     with gr.Tab("Data"):
         data_tab.load()
 
@@ -15,4 +15,8 @@ with gr.Blocks(theme='gstaff/xkcd') as gui:
     with gr.Tab("Train"):
         train_tab.load()
 
-gui.queue().launch()
+gui.queue()
+gui.launch(
+    server_name="0.0.0.0",
+    server_port=7861,
+)
